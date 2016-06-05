82ded8715807059f529a200c6d4fa704528c8a5a
Artemij Fedosejev <artemij.fedosejev@gmail.com>
2016-06-05 16:53:23 +0100

    Update

---
+++ source/js/components/Application.jsx
@@ -0,0 +1,120 @@
+var React = require('react');
+
+var INITIAL_LOCATION = {
+  address: 'London, United Kingdom',
+  position: {
+    latitude: 51.5085300,
+    longitude: -0.1257400
+  }
+};
+
+var Application = React.createClass({
+
+  map: null,
+  geocoder: null,
+  marker: null,
+  
+  getInitialState: function () {
+    return {
+      isGeocodingError: false,
+      foundAddress: INITIAL_LOCATION.address
+    };
+  },
+
+  geocodeAddress: function (address) {
+    this.geocoder.geocode({ 'address': address }, function handleResults(results, status) {
+
+      if (status === google.maps.GeocoderStatus.OK) {
+
+        this.setState({
+          foundAddress: results[0].formatted_address,
+          isGeocodingError: false
+        });
+
+        this.map.setCenter(results[0].geometry.location);
+        this.marker.setPosition(results[0].geometry.location);
+
+        return;
+      }
+
+      this.setState({
+        isGeocodingError: true
+      });
+
+    }.bind(this));
+  },
+
+  handleFormSubmit: function (submitEvent) {
+    submitEvent.preventDefault();
+
+    var address = this.refs.address.value;
+
+    this.geocodeAddress(address);
+  },
+
+  componentDidMount: function () {
+    var mapElement = this.refs.map;
+    
+    this.map = new google.maps.Map(mapElement, {
+      zoom: 8,
+      center: {
+        lat: INITIAL_LOCATION.position.latitude,
+        lng: INITIAL_LOCATION.position.longitude
+      }
+    });
+
+    this.marker = new google.maps.Marker({
+      map: this.map,
+      position: {
+        lat: INITIAL_LOCATION.position.latitude,
+        lng: INITIAL_LOCATION.position.longitude
+      }
+    });
+
+    this.geocoder = new google.maps.Geocoder();
+  },
+
+  render: function () {
+    return (
+      <div className="container">
+
+        <div className="row">
+          <div className="col-sm-12">
+
+            <form className="form-inline" onSubmit={this.handleFormSubmit}>
+              <div className="row">
+                <div className="col-sm-10">
+
+                  <div className="form-group">
+                    <label className="sr-only" htmlFor="address">Address</label>
+                    <input type="text" className="form-control input-lg" id="address" placeholder="London" ref="address" required />
+                  </div>
+
+                </div>
+                <div className="col-sm-2">
+
+                  <button type="submit" className="btn btn-default btn-lg">
+                    <span className="glyphicon glyphicon-search" aria-hidden="true"></span>
+                  </button>
+
+                </div>
+              </div>
+            </form>
+
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-sm-12">
+
+            {this.state.isGeocodingError ? <p className="bg-danger">Address not found.</p> : <p className="bg-info">{this.state.foundAddress}</p>}
+
+            <div ref="map" className="map"></div>
+
+          </div>
+        </div>
+      </div>
+    );
+  }
+});
+
+module.exports = Application;
