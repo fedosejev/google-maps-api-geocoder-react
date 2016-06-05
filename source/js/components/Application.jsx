fd350a617684a8a8c34c68ab39eba339dee28c01
Artemij Fedosejev <artemij.fedosejev@gmail.com>
2016-06-05 17:51:28 +0100

    Update

--- source/js/components/Application.jsx
+++ source/js/components/Application.jsx
@@ -101,7 +101,7 @@ var Application = React.createClass({
 
             <form className="form-inline" onSubmit={this.handleFormSubmit}>
               <div className="row">
-                <div className="col-xs-10">
+                <div className="col-xs-9">
 
                   <div className="form-group">
                     <label className="sr-only" htmlFor="address">Address</label>
@@ -109,7 +109,7 @@ var Application = React.createClass({
                   </div>
 
                 </div>
-                <div className="col-xs-2">
+                <div className="col-xs-3">
 
                   <button type="submit" className="btn btn-default btn-lg">
                     <span className="glyphicon glyphicon-search" aria-hidden="true"></span>
