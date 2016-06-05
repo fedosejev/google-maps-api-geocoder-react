1cc54e70f2f00999bf3c12ad4a56d2ee151ab875
Artemij Fedosejev <artemij.fedosejev@gmail.com>
2016-06-05 17:23:12 +0100

    Update

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+# React needs this for production build
 export NODE_ENV=production
 
+# Run Gulp task
 gulp build-for-production
 
+# Commit
 git commit -a -m "Update" && git push
+
+# Publish on GitHub Pages
 git subtree push --prefix build origin gh-pages
